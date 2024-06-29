a564f23ca2f88621c8194e1f40ca27c08ea2818c
kumarmanojgit <krmanoj72099@gmail.com>
2024-06-29 18:40:45 +0530

    add userschema and model

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/usersSchema");
 const home = async (req, res) => {
   try {
     res.status(200).json({ message: "welocome to the new world" });
@@ -8,10 +9,13 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+    const userExsit = await User.findOne({ email }); // already email exist or not
+    if (userExist) return res.status(400).json({ msg: "email already exit" });
+    const userCreated = await User.create({ username, email, phone, password });
     res.status(200).json({ message: req.body });
   } catch (error) {
-    console.log("error");
+    console.log("internal error");
   }
 };
 
